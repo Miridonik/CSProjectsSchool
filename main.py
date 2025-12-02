bb82c539234bc8d577e15ed89f297869ce65ee48
Kostya <kostymiridonov@gmail.com>
2025-12-02 14:52:28 +0300

    First-lesson

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import pandas
-print("hello world")
+
+data = pandas.read_csv("wine.csv")
+data = data.drop("region_1",axis=1)
+data = data.dropna(subset="country")
+mean_price = data["price"].sum()/data["price"].count()
+data["price"] = data["price"].fillna(mean_price)
+#print(data.describe())
+#print(data.isna().sum())
+#print(data[ (data["price"]>300) & (data["country"]=="Italy") ])
+print(data.groupby(["country","price"]).count())
