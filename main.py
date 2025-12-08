db37e9a6311fd52ee599a91e8f130672c9c8cdd4
Kostya <kostymiridonov@gmail.com>
2025-12-09 00:03:00 +0300

    HomeWork 09.12.2025

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-import pandas
+import pandas as pd
 
-data = pandas.read_csv("wine.csv")
-data = data.drop("region_1",axis=1)
-data = data.dropna(subset="country")
-mean_price = data["price"].sum()/data["price"].count()
-data["price"] = data["price"].fillna(mean_price)
-#print(data.describe())
+#Class work
+# data = pandas.read_csv("wine.csv")
+# data = data.drop("region_1",axis=1)
+# data = data.dropna(subset="country")
+# mean_price = data["price"].sum()/data["price"].count()
+# data["price"] = data["price"].fillna(mean_price)
+# #print(data.describe())
+# #print(data.isna().sum())
+# #print(data[ (data["price"]>300) & (data["country"]=="Italy") ])
+# print(data.groupby(["country","price"]).count())
+
+#Home
+data = pd.read_csv("titanic.csv")
+
+#data = data.dropna()
 #print(data.isna().sum())
-#print(data[ (data["price"]>300) & (data["country"]=="Italy") ])
-print(data.groupby(["country","price"]).count())
+#print(data.columns)
+#print(data[data.Pclass == 1].Fare.mean())
+print(data[data.Survived == 0].Age.mean())
